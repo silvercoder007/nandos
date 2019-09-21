3a1966264c60977a0db62682e845f93afcff2a15
Ali K <akayaniwork@outlook.com>
2019-09-21 04:22:54 +0100

    marsrover - added jest to project

---
+++ testdata/data.js
@@ -0,0 +1,13 @@
+const data = {
+  grid: "5 5",
+  rovers: [
+    {
+      startCoords: "1 2 N",
+      commands: "LMLMLMLMM"
+    },
+    {
+      startCoords: "3 3 E",
+      commands: "MMRMMRMRRM"
+    }
+  ]
+};
